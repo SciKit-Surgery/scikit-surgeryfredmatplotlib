27894d98dfea3e25a8810a7c224250e716eaf3e7
Stephen Thompson <s.thompson@ucl.ac.uk>
2020-06-23 10:17:20 +0100

    Issue #8 removed unused tests

--- tests/algorithms/test_errors.py
+++ tests/algorithms/test_errors.py
@@ -14,9 +14,10 @@ def _eav_by_brute_force(stddevs):
 
     cum_sum = np.zeros(np.array(stddevs).shape, dtype=np.float64)
     for _ in range(1000):
-        cum_sum += np.absolute(np.random.normal(
+        error = np.random.normal(
             loc=np.zeros(np.array(stddevs).shape),
-            scale=stddevs))
+            scale=stddevs)
+        cum_sum += error * error
 
     eavs = cum_sum/1000.0
     eav = np.linalg.norm(eavs)
@@ -30,10 +31,6 @@ def test_expected_absolute_value_1d():
     np.random.seed(0)
     stddevs = np.array([0.0], dtype=np.float64)
 
-    stddevs[0] = -1.0
-    with pytest.raises(ValueError):
-        eav = e2d.expected_absolute_value(stddevs)
-
     stddevs[0] = 0.0
     eav = e2d.expected_absolute_value(stddevs)
 
@@ -43,20 +40,20 @@ def test_expected_absolute_value_1d():
     stddevs[0] = 1.0
     eav = e2d.expected_absolute_value(stddevs)
 
-    arith_eav = np.linalg.norm(stddevs) * math.sqrt(2.0/math.pi)
+    arith_eav = np.linalg.norm(stddevs * stddevs)
     assert np.isclose(eav, arith_eav, atol=0.0, rtol=0.0001)
     #absolute(a - b) <= (atol + rtol * absolute(b))
     #so rtol = 0.1 is around 10% rtol = 0.01 is 1 %
-    assert np.isclose(eav, _eav_by_brute_force(stddevs), atol=0.0, rtol=0.05)
+    assert np.isclose(eav, _eav_by_brute_force(stddevs), atol=0.0, rtol=0.15)
 
     for _ in range(10):
         stddevs[0] = np.absolute(np.random.normal()*100.0)
         eav = e2d.expected_absolute_value(stddevs)
 
-        arith_eav = np.linalg.norm(stddevs) * math.sqrt(2.0/math.pi)
+        arith_eav = np.linalg.norm(stddevs * stddevs)
         assert np.isclose(eav, arith_eav, atol=0.0, rtol=0.0001)
         assert np.isclose(eav, _eav_by_brute_force(stddevs),
-                          atol=0.0, rtol=0.05)
+                          atol=0.0, rtol=0.15)
 
 
 def test_expected_absolute_value_2d():
@@ -66,30 +63,26 @@ def test_expected_absolute_value_2d():
     np.random.seed(0)
     stddevs = np.array([0.0, 0.0], dtype=np.float64)
 
-    stddevs[1] = -1.0
-    with pytest.raises(ValueError):
-        eav = e2d.expected_absolute_value(stddevs)
-
     stddevs = [0.0, 0.0]
     eav = e2d.expected_absolute_value(stddevs)
 
     assert eav == 0.0
     assert eav == _eav_by_brute_force(stddevs)
 
-    stddevs = [1.0, 1.0]
+    stddevs = np.array([1.0, 1.0], dtype=np.float64)
     eav = e2d.expected_absolute_value(stddevs)
 
-    arith_eav = np.linalg.norm(stddevs) * math.sqrt(2.0/math.pi)
+    arith_eav = np.linalg.norm(stddevs * stddevs)
     assert np.isclose(eav, arith_eav, atol=0.0, rtol=0.0001)
     assert np.isclose(eav, _eav_by_brute_force(stddevs), atol=0.0, rtol=0.05)
 
     for _ in range(10):
         stddevs = np.absolute(np.random.normal((1, 1))*100.0)
         eav = e2d.expected_absolute_value(stddevs)
-        arith_eav = np.linalg.norm(stddevs) * math.sqrt(2.0/math.pi)
+        arith_eav = np.linalg.norm(stddevs * stddevs)
         assert np.isclose(eav, arith_eav, atol=0.0, rtol=0.0001)
         assert np.isclose(eav, _eav_by_brute_force(stddevs),
-                          atol=0.0, rtol=0.05)
+                          atol=0.0, rtol=0.15)
 
 
 
@@ -100,139 +93,23 @@ def test_expected_absolute_value_3d():
     np.random.seed(0)
     stddevs = np.array([0.0, 0.0, 0.0], dtype=np.float64)
 
-    stddevs[1] = -1.0
-    with pytest.raises(ValueError):
-        eav = e2d.expected_absolute_value(stddevs)
-
-    stddevs = [0.0, 0.0, 0.0]
     eav = e2d.expected_absolute_value(stddevs)
 
     assert eav == 0.0
     assert eav == _eav_by_brute_force(stddevs)
 
-    stddevs = [1.0, 1.0, 1.0]
+    stddevs = np.array([1.0, 1.0, 1.0], dtype=np.float64)
     eav = e2d.expected_absolute_value(stddevs)
 
-    arith_eav = np.linalg.norm(stddevs) * math.sqrt(2.0/math.pi)
+    arith_eav = np.linalg.norm(stddevs * stddevs)
     assert np.isclose(eav, arith_eav, atol=0.0, rtol=0.0001)
-    assert np.isclose(eav, _eav_by_brute_force(stddevs), atol=0.0, rtol=0.05)
+    assert np.isclose(eav, _eav_by_brute_force(stddevs), atol=0.0, rtol=0.15)
 
     for _ in range(10):
         stddevs = np.absolute(np.random.normal((1, 1, 1))*100.0)
         eav = e2d.expected_absolute_value(stddevs)
-        arith_eav = np.linalg.norm(stddevs) * math.sqrt(2.0/math.pi)
+        arith_eav = np.linalg.norm(stddevs * stddevs)
         assert np.isclose(eav, arith_eav, atol=0.0, rtol=0.0001)
         assert np.isclose(eav, _eav_by_brute_force(stddevs),
-                          atol=0.0, rtol=0.05)
+                          atol=0.0, rtol=0.15)
 
-
-def test_compute_tre_from_fle_2d():
-    """
-    Tests for tre_from_fle_2d
-    """
-
-    stddevs = np.array([1.0, 1.0] , dtype=np.float64)
-    mean_fle_squared = e2d.expected_absolute_value(stddevs)
-    #three points on a unit circle
-    angle0 = 0.0
-    angle1 = math.pi * 2.0/3.0
-    angle2 = math.pi * 4.0/3.0
-    radius = 1.0
-
-    fiducials = np.array([[radius * math.cos(angle0) , radius * math.sin(angle0)],
-                          [radius * math.cos(angle1) , radius * math.sin(angle1)],
-                          [radius * math.cos(angle2) , radius * math.sin(angle2)]])
-
-    target = np.array([[0.0, 0.0, 0.0]])
-
-    target_dist_from_principal_axis = [0.0, 0.0]
-    rms_fiducial_dist_from_principal_x_axis = math.sqrt(
-        (fiducials[0][0]*fiducials[0][0] + 
-         fiducials[1][0]*fiducials[1][0] + 
-         fiducials[2][0]*fiducials[2][0])/3.0)
-    rms_fiducial_dist_from_principal_y_axis = math.sqrt(
-        (fiducials[0][1]*fiducials[0][1] + 
-         fiducials[1][1]*fiducials[1][1] + 
-         fiducials[2][1]*fiducials[2][1])/3.0)
-
-    moment_of_inerta = (target_dist_from_principal_axis[0] *
-                        target_dist_from_principal_axis[0] /
-                        (rms_fiducial_dist_from_principal_x_axis * 
-                         rms_fiducial_dist_from_principal_x_axis) +
-                        target_dist_from_principal_axis[1] *
-                        target_dist_from_principal_axis[1] /
-                        (rms_fiducial_dist_from_principal_y_axis *
-                         rms_fiducial_dist_from_principal_y_axis))/2.0
-    tre_squared = mean_fle_squared / float(fiducials.shape[1]) * ( 
-        1.0 + moment_of_inerta)
-
-    print (tre_squared)
-    assert e2d.compute_tre_from_fle_2d(fiducials, mean_fle_squared, target[:, 0:2]) == tre_squared
-
-    #next we try it with a brute force simulation
-    rotation, translation, fre = orthogonal_procrustes(
-                fixed_points, moving_points)
-    
-    transformed_target = np.matmul(rotation,
-                                           target.transpose()) + \
-                                           translation
-    
-    transformed_target_2d = [transformed_target[0][0],
-                                     transformed_target[1][0]]
-
-    actual_tre = np.linalg.norm(
-                transformed_target_2d - self.target[:, 0:2])
-
-
-def test_compute_tre_from_fle_2d_brute_force():
-    """
-    Tests for tre_from_fle_2d
-    """
-
-    stddevs = np.array([[1.0, 1.0, 0.0],[1.0, 1.0, 0.0],[1.0, 1.0, 0.0]] , dtype=np.float64)
-    mean_fle_squared = e2d.expected_absolute_value(np.array([1.0,1.0]))
-    #three points on a unit circle
-    angle0 = 0.0
-    angle1 = math.pi * 2.0/3.0
-    angle2 = math.pi * 4.0/3.0
-    radius = 1.0
-
-    fiducials = np.array([[radius * math.cos(angle0) , radius * math.sin(angle0), 0.0],
-                          [radius * math.cos(angle1) , 1.0 * math.sin(angle1), 0.0],
-                          [radius * math.cos(angle2) , 1.0 * math.sin(angle2), 0.0]])
-
-    print(fiducials)
-
-    target = np.array([[0.0, 0.0, 0.0]])
-
-    
-    tre_sum = 0
-    tre_sum_sqr = 0
-    count = 0
-    for _ in range (1000):
-        moving_fids = fiducials
-        fixed_fids = fiducials + np.random.normal(scale=stddevs)
-    #next we try it with a brute force simulation
-        rotation, translation, fre = orthogonal_procrustes(
-                fixed_fids, moving_fids)
-    
-        transformed_target = np.matmul(rotation,
-                                       target.transpose()) + \
-                                       translation
-    
-        transformed_target_2d = [transformed_target[0][0],
-                                     transformed_target[1][0]]
-
-        actual_tre = np.linalg.norm(
-                transformed_target_2d - target[:, 0:2])
-        
-        tre_sum_sqr += actual_tre*actual_tre
-        tre_sum += actual_tre
-        count += 1
-
-    mean_tre = tre_sum/float(count)
-    mean_tre_sqr = tre_sum_sqr/float(count)
-    tre_mean_sqr = mean_tre * mean_tre
-    print(mean_tre_sqr)
-    print (tre_mean_sqr)
-    print (e2d.compute_tre_from_fle_2d(fiducials[:,0:2], mean_fle_squared, target[:, 0:2]))
