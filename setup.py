a404c5c3ea655d1ce1c807fe152b0b5b9742c99e
Stephen Thompson <s.thompson@ucl.ac.uk>
2022-03-08 11:24:17 +0000

    Issue #47 hold back matplot lib version due to all_axes API change

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
 
     install_requires=[
             'numpy',
-            'matplotlib',
+            'matplotlib<3.3.3',
             'scikit-image>0.15',
             'scikit-surgeryfred>=0.0.9',
             'ipykernel',
